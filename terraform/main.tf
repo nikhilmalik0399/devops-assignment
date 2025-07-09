3a4ff2cda81d958006396f66345b50cc5b65d220
nikhilmalik0399 <nikhilmalik0399@gmail.com>
2025-07-09 19:16:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,33 @@
-provider "aws" {
-  region = "ap-south-1"  
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = [var.node_instance_type]
+      desired_size   = var.desired_capacity
+    }
+  }
+
+  enable_irsa = true
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  name    = "devops-vpc"
+  cidr    = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
 }
