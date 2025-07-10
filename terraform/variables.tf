1d29b445fcab54df15ead7ebd1da79b550ebac4c
nikhilmalik0399 <nikhilmalik0399@gmail.com>
2025-07-10 18:55:52 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,4 @@
-variable "cluster_name" {
-  default = "devops-eks-cluster"
-}
-
-variable "region" {
-  default = "ap-south-1"
-}
-
-variable "node_instance_type" {
-  default = "t3.medium"
-}
-
-variable "desired_capacity" {
-  default = 2
-}
+variable "cluster_name" { default = "devops-eks-cluster" }
+variable "region" { default = "ap-south-1" }
+variable "node_instance_type" { default = "t3.medium" }
+variable "desired_capacity" { default = 2 }
